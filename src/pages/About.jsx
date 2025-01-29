45fdb568f270321efeb6b88efe4431ba3c3e9015
unknown <danrlei_vieira@hotmail.com>
2025-01-29 08:46:56 -0300

    feat: Added content to the about and planets page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,43 +1,65 @@
+import styles from "./about.module.css";
+
 function About() {
   return (
-    <>
-      <h2>Sobre o projeto: </h2>
-      <p>
-        Este projeto esta consumindo uma API de Dragon Ball feita pelo{" "}
-        <strong>Antonio Alvarez</strong>e pode ser encontrada no link:
-        <a href="https://web.dragonball-api.com/">AQUI</a>
-      </p>
-      <p>
-        A API Dragon Ball é uma API REST abrangente inspirada na icônica série
-        de televisão Dragon Ball. Esta API concede aos usuários acesso a um
-        extenso banco de dados com centenas de personagens, imagens,
-        transformações e planetas do universo Dragon Ball. Ela abrange
-        informações canônicas derivadas de várias séries, incluindo Dragon Ball
-        Z, Dragon Ball GT, Dragon Ball Super, bem como filmes e um toque de
-        Dragon Ball Heroes.
-      </p>
-      <h2>Quem é você?</h2>
-      <p>
-        Sou Danrlei Vieira, um profissional com muitos sonhos e vontade de
-        aprender e vencer. E este projeto é mais um feito para testar minhas
-        habilidades e conhecimentos.
-      </p>
-      <h2>Direitos autorais</h2>
-      <p>
-        Todos os personagens, imagens e conteúdo relacionado de Dragon Ball
-        apresentados neste projeto são propriedade intelectual de seus
-        respectivos criadores, Akira Toriyama e Toei Animation. Reconhecemos e
-        respeitamos seu trabalho criativo. Este projeto é uma iniciativa criada
-        por fãs e não se destina a fins comerciais. Nenhuma violação de direitos
-        autorais é pretendida, e todos os direitos de Dragon Ball pertencem aos
-        seus legítimos proprietários. Este projeto é de código aberto e aceita
-        contribuições da comunidade. Você pode encontrar o código-fonte neste
-        link . Sinta-se à vontade para explorar, contribuir e aprimorar o
-        projeto enquanto celebramos coletivamente nosso amor por Dragon Ball.
-        Verifique a licença do projeto para obter mais detalhes sobre uso e
-        distribuição.
-      </p>
-    </>
+    <div className={styles.parent}>
+      <div className={styles.container}>
+        <h2>Sobre o projeto: </h2>
+        <p className={styles.pAbout}>
+          Este projeto esta consumindo uma API de Dragon Ball feita pelo{" "}
+          <strong>Antonio Alvarez</strong>e pode ser encontrada
+          <a
+            className={styles.linkAbout}
+            href="https://web.dragonball-api.com/"
+            target="_blank"
+          >
+            {" "}
+            AQUI.
+          </a>
+        </p>
+        <p className={styles.pAbout}>
+          A API Dragon Ball é uma API REST abrangente inspirada na icônica série
+          de televisão Dragon Ball. Esta API concede aos usuários acesso a um
+          extenso banco de dados com centenas de personagens, imagens,
+          transformações e planetas do universo Dragon Ball. Ela abrange
+          informações canônicas derivadas de várias séries, incluindo Dragon
+          Ball Z, Dragon Ball GT, Dragon Ball Super, bem como filmes e um toque
+          de Dragon Ball Heroes.
+        </p>
+        <h2>Quem é você?</h2>
+        <p className={styles.pAbout}>
+          Sou <strong>Danrlei Vieira,</strong> um profissional com muitos sonhos
+          e vontade de aprender e vencer. E este projeto é mais um feito para
+          testar minhas habilidades e conhecimentos.
+        </p>
+        <h2>Direitos autorais</h2>
+        <p className={styles.pAbout}>
+          Todos os personagens, imagens e conteúdo relacionado de Dragon Ball
+          apresentados neste projeto são propriedade intelectual de seus
+          respectivos criadores, Akira Toriyama e Toei Animation. Reconhecemos e
+          respeitamos seu trabalho criativo. Este projeto é uma iniciativa
+          criada por fãs e não se destina a fins comerciais. Nenhuma violação de
+          direitos autorais é pretendida, e todos os direitos de Dragon Ball
+          pertencem aos seus legítimos proprietários.
+        </p>
+        <p className={styles.pAbout}>
+          Este projeto é de código aberto e aceita contribuições da comunidade.
+          Você pode encontrar o código-fonte{" "}
+          <a
+            className={styles.linkAbout}
+            href="https://github.com/Danrlei22/api-dragon-ball.git"
+            target="_blank"
+          >
+            {" "}
+            AQUI.
+          </a>{" "}
+          Sinta-se à vontade para explorar, contribuir e aprimorar o projeto
+          enquanto celebramos coletivamente nosso amor por Dragon Ball.
+          Verifique a licença do projeto para obter mais detalhes sobre uso e
+          distribuição.
+        </p>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Planets.jsx
+++ src/pages/Planets.jsx
@@ -1,9 +1,87 @@
-
+import { useEffect } from "react";
+import styles from "./planets.module.css";
+import { useState } from "react";
 
 function Planets() {
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const totalPages = 10;
+
+  useEffect(() => {
+    const fetchCharacters = async (page) => {
+      const apiUrl = `https://dragonball-api.com/api/characters?page=${page}&limit=10`;
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error("Erro ao buscar dados");
+      }
+      return response.json();
+    };
+
+    const fetchAllPlanets = async () => {
+      const uniquePlanets = [];
+
+      for (let page = 1; page <= totalPages; page++) {
+        const data = await fetchCharacters(page);
+
+        const characterPromises = data.items.map(async (character) => {
+          const characterResponse = await fetch(
+            `https://dragonball-api.com/api/characters/${character.id}`
+          );
+          const characterData = await characterResponse.json();
+          const planet = characterData.originPlanet;
+
+          if (planet) {
+            console.log(`${character.id} - ${character.name} Planeta encontrado: ${planet.name}`);
+            if (!uniquePlanets.some((p) => p.id === planet.id)) {
+              uniquePlanets.push(planet);
+            } 
+          }
+        });
+
+        await Promise.all(characterPromises);
+      }
+
+      setPlanets(uniquePlanets);
+      setLoading(false);
+    };
+    fetchAllPlanets().catch((error) => {
+      setError(error.message);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <p>Carregando planetas...</p>;
+  }
+
+  if (error) {
+    return <p>Erro ao carregar planetas: {error}</p>;
+  }
+
   return (
-    <div>
-      <h1>Planets</h1>
+    <div className={styles.containerPlanets}>
+      <h1 className={styles.h1Planet}>Planetas</h1>
+      {planets.map((planet) => (
+        <div key={planet.id} className={styles.planet}>
+          <h2>Planeta - {planet.name}</h2>
+          <p>
+            <strong className={styles.infoPlanet}>Destruído:</strong>
+            {planet.isDestroyed ? "Sim" : "Não"}
+          </p>
+          <img
+            src={planet.image}
+            alt={planet.name}
+            className={styles.imgPlanet}
+          />
+          <p>
+            <strong className={styles.infoPlanet}>Descrição:</strong>{" "}
+            {planet.description}
+          </p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
